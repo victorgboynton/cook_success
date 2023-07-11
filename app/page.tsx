51743cf7c64eb4e57d85eafba9f9d397da83a31b
victorgboynton <victorgboynton@gmail.com>
2023-07-11 02:32:26 -0600

    I forgot

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Navbar } from "./Navbar";
 export default function Home() {
 	return (
 		<div className="">
-			<div className="flex text-6xl bg-green-100 font-dmSerif rounded text-center lg:mt-20 justify-center items-center border-4 mx-auto p-8 max-w-6xl border-black">
+			<div className="flex text-6xl bg-green-200 font-bitter font-bold text-center lg:mt-10 justify-center items-center mx-auto p-8 ">
 				<h1>
 					<i>
 						<u>Never</u>
@@ -12,7 +12,7 @@ export default function Home() {
 					have left over ingredients again
 				</h1>
 			</div>
-			<div className="flex text-5xl font-dmSerif border-2 bg-green-50 border-black max-w-3xl rounded text-center justify-center items-center  mx-auto lg:m-20">
+			<div className="flex text-5xl font-bitter border-2 bg-green-200 font-semibold border-black max-w-3xl rounded text-center justify-center items-center  mx-auto sm:mt-8">
 				<h2>Ingredient of the Week!</h2>
 			</div>
 		</div>
