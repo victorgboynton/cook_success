ac6d76147935e3cbd450b97552bbbce57b3bf1c2
Victor <victorgboynton@gmail.com>
2023-07-12 00:38:25 -0600

    Changes to login template

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,13 +1,19 @@
 export default function Login() {
-	return (
-		<div>
-			<div>
-				<h1>Welcome!</h1>
-				<form>
-					<input type="text" placeholder="Username" />
-					<input type="text" placeholder="Password" />
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-screen items-center justify-center mx-auto my-auto">
+      <div className="bg-green-500 p-20 rounded-xl">
+        <h1 className="text-center text-xl">Welcome!</h1>
+        <form className="flex flex-col">
+          <p>Username: </p>
+          <input type="text" placeholder="Username" className="rounded" />
+          <p className="">Password:</p>
+          <input type="password" placeholder="Password" className="rounded" />
+          <div className="flex flex-row text-xs">
+            <button>Forgot Username? </button>
+            <button>Forgot Password?</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,18 +1,18 @@
 import { NavButton } from "./NavButton";
 
 export function Navbar() {
-	return (
-		<div className="grid grid-cols-3 px-2 items-center bg-green-300">
-			<div className="">
-				<NavButton linkTarget="/Login">Home</NavButton>
-			</div>
-			<h1 className=" items-center text-3xl mx-auto font-pacifico font-extrabold">
-				CookSuccess
-			</h1>
-			<div className=" flex justify-end gap-2">
-				<NavButton linkTarget="/Login">Saved Recipes</NavButton>
-				<NavButton linkTarget="/Login">Login</NavButton>
-			</div>
-		</div>
-	);
+  return (
+    <div className="grid grid-cols-3 px-2 items-center bg-green-300">
+      <div className="">
+        <NavButton linkTarget="/">Home</NavButton>
+      </div>
+      <h1 className=" items-center text-3xl mx-auto font-pacifico font-extrabold">
+        CookSuccess
+      </h1>
+      <div className=" flex justify-end gap-2">
+        <NavButton linkTarget="/Login">Saved Recipes</NavButton>
+        <NavButton linkTarget="/Login">Login</NavButton>
+      </div>
+    </div>
+  );
 }
