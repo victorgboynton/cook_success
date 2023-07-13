fc719b77e81748f887b1cc654fe9e1e959000306
victorgboynton <victorgboynton@gmail.com>
2023-07-12 20:40:40 -0600

    Initial Setup of mongodb

--- app/RecipePicker/page.tsx
+++ app/RecipePicker/page.tsx
@@ -1,14 +1,14 @@
 export default function RecipePicker() {
-  return (
-    <div className="">
-      <div className="grid grid-cols-2 mx-60">
-        <div className="bg-slate-500 h-screen">
-          <div></div>
-        </div>
-        <div className="bg-green-500 h-screen">
-          <h1>World</h1>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="">
+			<div className="grid grid-cols-2 mx-60">
+				<div className="bg-slate-500 h-screen">
+					<div>Hello</div>
+				</div>
+				<div className="bg-green-500 h-screen">
+					<h1>World</h1>
+				</div>
+			</div>
+		</div>
+	);
 }
